c6404839abd5aa13d841ddab4ed6f93b40242522
carmen <carmen@caarmen>
2024-11-07 10:01:44 +0100

    Descarga de los genes de HPO

---
+++ code/launch.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+# variables de entorno R_LIB, PYTHON_LIB
+#export PYTHON_LIB=./Py_libs
+
+# DESCARGA DE DATOS
+mkdir -p data
+# obtener genes de HPO
+curl -X 'GET' 'https://ontology.jax.org/api/network/annotation/HP%3A0002145/download/gene' -H 'accept: application/json' -o data/hpoGenes.tsv -s
+awk 'NR > 1{print $2}' data/hpoGenes.tsv > data/hpoGenesNames.tsv # obtener nombres de los genes
+
+
