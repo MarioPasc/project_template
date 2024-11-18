26ef288dba9999611c948b9d88c06870ea575aa2
MarioPasc <mario.pg02@gmail.com>
2024-11-18 14:49:09 +0100

    Refactor PyLibs path

--- code/networkAnalysis.py
+++ code/networkAnalysis.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import sys
-sys.path.append('../Py_libs')
+sys.path.append('./Py_libs')
 import igraph as ig
 from igraph import Graph
 import pandas as pd
--- code/obtainPPINetwork.py
+++ code/obtainPPINetwork.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import sys
-sys.path.append('../Py_libs')
+sys.path.append('./Py_libs')
 import argparse
 import stringdb
 from typing import Union, List
