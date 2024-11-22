86bac84f32d4422d15f27b6721a2229fb61006eb
MarioPasc <mario.pg02@gmail.com>
2024-11-22 22:57:38 +0100

    Refactorize Gonzalo's metrics to add a logger instead of a log_file str

--- code/clustering/metrics.py
+++ code/clustering/metrics.py
@@ -11,7 +11,7 @@ import requests
 
 class Metrics:
     @staticmethod
-    def modularity(graph: Graph, clusters: List[List[int]], log_file: Optional[str] = None) -> Optional[float]:
+    def modularity(graph: Graph, clusters: List[List[int]], logger: logging.Logger) -> Optional[float]:
         """
         Calcula la modularidad (Q) usando igraph y registra eventos importantes en un archivo de registro.
 
@@ -20,16 +20,6 @@ class Metrics:
         :param log_file: Ruta a un archivo de registro donde se escriben eventos importantes.
         :return: Un valor flotante representando la modularidad o None si ocurre un error.
         """
-        # Configurar logging
-        if log_file:
-            logging.basicConfig(
-                filename=log_file,
-                level=logging.INFO,
-                format='%(asctime)s - %(levelname)s - %(message)s'
-            )
-        else:
-            logging.basicConfig(level=logging.CRITICAL)  # Si no hay log_file, no registra nada.
-
         try:
             # Validar que el grafo sea un objeto de tipo igraph.Graph
             if not isinstance(graph, Graph):
@@ -56,15 +46,15 @@ class Metrics:
 
             # Calcular la modularidad usando igraph
             modularity_value = graph.modularity(membership)
-            logging.info(f"Modularidad calculada exitosamente: {modularity_value}")
+            logger.info(f"Modularidad calculada exitosamente: {modularity_value}")
             return modularity_value
 
         except Exception as e:
-            logging.error(f"Error al calcular la modularidad: {e}")
+            logger.error(f"Error al calcular la modularidad: {e}")
             return None
 
     @staticmethod
-    def functional_enrichment_score(graph: Graph, clusters: List[List[int]], log_file: Optional[str]) -> float:
+    def functional_enrichment_score(graph: Graph, clusters: List[List[int]], logger: logging.Logger) -> float:
         """
         Calcula una métrica de enriquecimiento funcional para los clústeres en un grafo.
 
@@ -73,43 +63,43 @@ class Metrics:
         :param log_file: Ruta del archivo de logging donde se escribirán los resultados.
         :return: Valor flotante promedio del puntaje de enriquecimiento funcional para todos los clústeres.
         """
-        logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+        # logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
         try:
             # Validar entradas
             Metrics._validate_inputs(graph, clusters)
 
-            logging.info("Iniciando cálculo del puntaje de enriquecimiento funcional.")
+            logger.info("Iniciando cálculo del puntaje de enriquecimiento funcional.")
 
             scores = []
             for i, cluster in enumerate(clusters):
                 # Obtener genes del clúster
                 genes = [graph.vs[node]["name"] for node in cluster if "name" in graph.vs[node].attributes()]
                 if not genes:
-                    logging.warning(f"Clúster {i} vacío o sin genes válidos. Se omite.")
+                    logger.warning(f"Clúster {i} vacío o sin genes válidos. Se omite.")
                     continue
 
                 # Enriquecimiento funcional para el clúster
-                enrichment_results = Metrics._perform_enrichment(genes, log_file)
+                enrichment_results = Metrics._perform_enrichment(genes=genes, logger=logger)
                 if enrichment_results is None:
                     continue
 
                 # Calcular el puntaje del clúster
                 cluster_score = Metrics._calculate_cluster_score(enrichment_results)
                 scores.append(cluster_score)
-                logging.info(f"Puntaje para clúster {i}: {cluster_score}")
+                logger.info(f"Puntaje para clúster {i}: {cluster_score}")
 
             # Calcular y retornar el promedio de los puntajes
             if scores:
                 average_score = sum(scores) / len(scores)
-                logging.info(f"Puntaje promedio de enriquecimiento funcional: {average_score}")
+                logger.info(f"Puntaje promedio de enriquecimiento funcional: {average_score}")
                 return average_score
             else:
-                logging.warning("No se calcularon puntajes válidos para los clústeres.")
+                logger.warning("No se calcularon puntajes válidos para los clústeres.")
                 return 0.0
 
         except Exception as e:
-            logging.error(f"Error en functional_enrichment_score: {e}")
+            logger.error(f"Error en functional_enrichment_score: {e}")
             raise
 
     @staticmethod
@@ -121,7 +111,7 @@ class Metrics:
             raise TypeError("El parámetro 'clusters' debe ser una lista de listas.")
 
     @staticmethod
-    def _perform_enrichment(genes: List[str], log_file: str) -> pd.DataFrame:
+    def _perform_enrichment(genes: List[str], logger: logging.Logger) -> pd.DataFrame:
         """
         Realiza un análisis de enriquecimiento funcional para un conjunto de genes utilizando STRINGdb.
 
@@ -129,35 +119,34 @@ class Metrics:
         :param log_file: Ruta del archivo de logging donde se registrarán los resultados.
         :return: DataFrame con los resultados del enriquecimiento funcional o None si no hay resultados.
         """
-        logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
         try:
             # Validar entrada
             if not isinstance(genes, list) or not all(isinstance(gene, str) for gene in genes):
                 raise ValueError("El argumento 'genes' debe ser una lista de nombres de genes en formato string.")
 
-            logging.info(f"Realizando análisis de enriquecimiento para los genes: {genes}")
+            logger.info(f"Realizando análisis de enriquecimiento para los genes: {genes}")
 
             # Obtener IDs de STRINGdb
             string_ids = get_string_ids(genes)
             if string_ids.empty:
-                logging.warning("No se encontraron IDs válidos para los genes proporcionados.")
+                logger.warning("No se encontraron IDs válidos para los genes proporcionados.")
                 return None
             
-            logging.info(f"IDs de STRINGdb obtenidos: {string_ids['stringId'].tolist()}")
+            logger.info(f"IDs de STRINGdb obtenidos: {string_ids['stringId'].tolist()}")
 
             # Realizar análisis de enriquecimiento
             enrichment_results = get_enrichment(string_ids['stringId'].tolist())
             filtered_terms = enrichment_results[enrichment_results['category'] == 'Process']
             if filtered_terms.empty:
-                logging.warning("No se encontraron términos enriquecidos para los genes proporcionados.")
+                logger.warning("No se encontraron términos enriquecidos para los genes proporcionados.")
                 return None
 
-            logging.info(f"Resultados de enriquecimiento obtenidos: {len(filtered_terms)} términos enriquecidos.")
+            logger.info(f"Resultados de enriquecimiento obtenidos: {len(filtered_terms)} términos enriquecidos.")
             return filtered_terms
 
         except Exception as e:
-            logging.error(f"Error durante el análisis de enriquecimiento funcional: {e}")
+            logger.error(f"Error durante el análisis de enriquecimiento funcional: {e}")
             raise
 
     @staticmethod
