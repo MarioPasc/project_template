80e2bac454b816924cb6f158bd87f6ddc19e37b4
ainhoaPer <ainhoapergon@uma.es>
2024-11-30 11:33:48 +0100

    Mejora del gráfico de barras

--- code/functional_analysis/visualization.py
+++ code/functional_analysis/visualization.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 import networkx as nx
+from typing import List, Dict
 
 class FunctionalVisualization:
     @staticmethod
@@ -37,16 +39,36 @@ class FunctionalVisualization:
             print(f"Error en dot_plot: {e}")
 
     @staticmethod
-    def bar_plot(df: pd.DataFrame):
+    def bar_plot(df: pd.DataFrame, output_file:str = None):
         try:
-            df = df.sort_values('Adjusted P-value').head(10)
-            plt.barh(df['Term'], df['Adjusted P-value'], color='skyblue')
-            plt.title('Bar Plot')
-            plt.xlabel('Adjusted P-value')
-            plt.ylabel('Term')
+            # Ordenar los términos por significancia (los más significativos primero)
+            df = df.sort_values('Adjusted P-value', ascending=True).head(20)
+            
+            # Crear el gráfico de barras
+            plt.figure(figsize=(12, 8))
+            sns.barplot(
+                data=df,
+                x='Adjusted P-value',
+                y='Term',
+                hue='Term',  # Resolver FutureWarning
+                dodge=False,  # Prevenir separación innecesaria
+                palette='Spectral'  # Paleta más contrastante
+            )
+            plt.xscale('log')  # Usar escala logarítmica para resaltar diferencias
+            plt.title('Bar Plot - Enrichment Analysis', fontsize=14)
+            plt.xlabel('Adjusted P-value (log scale)', fontsize=12)
+            plt.ylabel('Term', fontsize=12)
+            plt.gca().yaxis.set_tick_params(labelsize=10)  # Tamaño de etiquetas
+            plt.tight_layout()
+            
+            # Guardar o mostrar el gráfico
+            if output_file:
+                plt.savefig(output_file, dpi= 300)
+                print(f"Gráfico guardado en {output_file}")
             plt.show()
-        except:
-            print("Error in bar_plot")
+            
+        except Exception as e:
+            print(f"Error en bar_plot: {e}")
 
     @staticmethod
     def cnet_plot(gene_sets):
