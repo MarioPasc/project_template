f7af553b00b12d7433121e9b055096b20a287163
ainhoaPer <ainhoapergon@uma.es>
2024-12-09 12:46:37 +0100

    Ajuste en la función de filtrado del análisis funcional para incluir overlap

--- code/functional_analysis/functional_analysis.py
+++ code/functional_analysis/functional_analysis.py
@@ -15,9 +15,7 @@ class FunctionalAnalysis:
         :param graph: Objeto grafo que ya incluye los clústeres y genes como nodos.
         """
         if not isinstance(graph, igraph.Graph):
-            raise TypeError(
-                "El argumento 'graph' debe ser un objeto de tipo igraph.Graph."
-            )
+            raise TypeError("El argumento 'graph' debe ser un objeto de tipo igraph.Graph.")
         self.graph = graph
 
     @staticmethod
@@ -35,17 +33,15 @@ class FunctionalAnalysis:
             # Realizar análisis funcional usando Enrichr
             enr = gp.enrichr(
                 gene_list=genes,
-                gene_sets="GO_Biological_Process_2021",  # Base de datos funcional, ajustable
-                organism="Human",  # Especificar organismo
+                gene_sets='GO_Biological_Process_2021',  # Base de datos funcional, ajustable
+                organism='Human',  # Especificar organismo
                 outdir=None,  # No guardar resultados en disco
-                no_plot=True,  # No generar gráficos
+                no_plot=True  # No generar gráficos
             )
 
             # Retornar resultados si hay datos
             if enr.results.empty:
-                return (
-                    pd.DataFrame()
-                )  # Retornar un DataFrame vacío si no hay resultados
+                return pd.DataFrame()  # Retornar un DataFrame vacío si no hay resultados
 
             return enr.results
 
@@ -53,9 +49,7 @@ class FunctionalAnalysis:
             print(f"Error en _perform_enrichment_analysis para genes {genes}: {e}")
             return pd.DataFrame()
 
-    def perform_analysis(
-        self, clustering_data: Dict[str, Any], output_file: str, algorithm: str = None
-    ):
+    def perform_analysis(self, clustering_data: Dict[str, Any], output_file: str, algorithm: str = None):
         """
         Realiza el análisis funcional para los clústeres extraídos de un JSON y guarda los resultados en un archivo CSV.
 
@@ -65,18 +59,14 @@ class FunctionalAnalysis:
         """
         try:
             if not isinstance(clustering_data, dict):
-                raise ValueError(
-                    "El argumento 'clustering_data' debe ser un diccionario."
-                )
+                raise ValueError("El argumento 'clustering_data' debe ser un diccionario.")
 
             all_results = []
 
             # Filtrar por algoritmo si se especifica
             if algorithm:
                 if algorithm not in clustering_data:
-                    raise ValueError(
-                        f"El algoritmo '{algorithm}' no está presente en los datos de clustering."
-                    )
+                    raise ValueError(f"El algoritmo '{algorithm}' no está presente en los datos de clustering.")
                 clustering_data = {algorithm: clustering_data[algorithm]}
 
             for key, clusters in clustering_data.items():
@@ -87,17 +77,15 @@ class FunctionalAnalysis:
                     enrichment_results = self._perform_enrichment_analysis(genes)
 
                     if enrichment_results.empty:
-                        print(
-                            f"No se encontraron resultados de enriquecimiento para {key} - {cluster_id}."
-                        )
+                        print(f"No se encontraron resultados de enriquecimiento para {key} - {cluster_id}.")
                         continue
 
-                    enrichment_results["Algorithm"] = key
-                    enrichment_results["Cluster"] = cluster_id
+                    enrichment_results['Algorithm'] = key
+                    enrichment_results['Cluster'] = cluster_id
                     all_results.append(enrichment_results)
 
             if all_results:
-                final_results = pd.concat(all_results, ignore_index=True)
+                final_results = pd.concat(all_results, ignore_index=True).drop(columns=["Old P-value", "Old Adjusted P-value"], errors="ignore")
                 final_results.to_csv(output_file, index=False)
                 print(f"Resultados guardados en {output_file}.")
             else:
@@ -106,40 +94,48 @@ class FunctionalAnalysis:
         except Exception as e:
             print(f"Error en perform_analysis: {e}")
 
-    def filter_results(
-        self,
-        input_file: str,
-        output_file: str,
-        p_value_threshold: float = None,
-        combined_score_min: float = None,
-    ):
+            
+    def filter_results(self, input_file: str, output_file: str, 
+                       p_value_threshold: float = None, 
+                       combined_score_min: float = None,
+                       overlap_percentage_min: float = None):
         """
-        Filtra los resultados del análisis funcional según p-valor o Combined Score.
+        Filtra los resultados del análisis funcional según p-valor ajustado, Combined Score o porcentaje de Overlap
+        (debe proporcionarse al menos un criterio de filtrado).
 
         :param input_file: Ruta al archivo CSV con los resultados del análisis funcional.
         :param output_file: Ruta al archivo CSV donde se guardarán los resultados filtrados.
         :param p_value_threshold: Umbral máximo del p-valor. (Opcional)
         :param combined_score_min: Umbral mínimo de Combined Score. (Opcional)
+        :param overlap_percentage_min: Umbral mínimo de porcentaje de Overlap. (Opcional)
         """
         try:
             # Cargar resultados
             data = pd.read_csv(input_file)
+            
+            # Validar que se proporciona al menos un criterio de filtrado
+            if p_value_threshold is None and combined_score_min is None and overlap_percentage_min is None:
+                raise ValueError("Debes proporcionar al menos un criterio de filtrado (p_value_threshold, combined_score_min o overlap_percentage_min).")
 
-            # Validar que se proporcione al menos un criterio de filtrado
-            if p_value_threshold is None and combined_score_min is None:
-                raise ValueError(
-                    "Debes proporcionar al menos un criterio de filtrado (p_value_threshold o combined_score_min)."
+            # Crear una nueva columna de Overlap_Percentage si se proporciona el umbral
+            if overlap_percentage_min is not None:
+                data["Overlap_Percentage"] = data["Overlap"].apply(
+                    lambda x: int(x.split("/")[0]) / int(x.split("/")[1])
                 )
-
+            
             # Aplicar filtros
             if p_value_threshold is not None:
-                data = data[data["P-value"] <= p_value_threshold]
+                data = data[data['Adjusted P-value'] <= p_value_threshold]
 
             if combined_score_min is not None:
-                data = data[data["Combined Score"] >= combined_score_min]
+                data = data[data['Combined Score'] >= combined_score_min]
+                
+            if overlap_percentage_min is not None:
+                data = data[data['Overlap_Percentage'] >= overlap_percentage_min]
 
             # Guardar resultados filtrados
             data.to_csv(output_file, index=False)
             print(f"Resultados filtrados guardados en {output_file}.")
+            
         except Exception as e:
             print(f"Error al filtrar resultados: {e}")
